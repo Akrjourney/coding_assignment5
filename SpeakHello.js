bd69066c7d0e2911189fab2b02e1859239743397
Akrjourney <akarikuma1993@gmail.com>
2022-06-10 21:08:55 +0900

    coding_assignment5

--- SpeakHello.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-    let helloSpeaker = {};
-    let speakWord = "Hello";
-
-    helloSpeaker.speak = function (names) {
-      console.log(speakWord + " " + names);
-    }
-        window.helloSpeaker = helloSpeaker;
-    })(window);
